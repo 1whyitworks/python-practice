05a988709157474a58086cb1784963ce5911e997
sasha <156845202+1whyitworks@users.noreply.github.com>
2025-02-28 11:52:38 +0300

    Update Problem-5.py

--- Problem-5.py
+++ Problem-5.py
@@ -6,6 +6,14 @@
 
 def fibonacci_sequence(n):
     # TODO: Return a list with the first n Fibonacci numbers
+    if n == 0:
+        return []
+    if n == 1:
+        return [0]
+    result = [0, 1]
+    for i in range(2, n):
+        result.append(result[i-1] + result[i-2])
+    return result
 
 # Test
 print(fibonacci_sequence(7))  # Expected output: [0, 1, 1, 2, 3, 5, 8]
